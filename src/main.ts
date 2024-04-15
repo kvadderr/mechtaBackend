8e56a286ba87b2012ef8ee6d20d0dd13000d03e9
Сергей Похабов <itigertechno@gmail.com>
2024-04-15 11:26:37 +0500

    first commit

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://lotos.na4u.ru', 'http://95.213.173.58:5173'],
+    origin: ['http://localhost:5173', 'http://78.155.194.209:5175', 'http://95.213.173.58:5173'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
